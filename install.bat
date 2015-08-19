8cb5fea3fcdc4a0e2e0ec1683e8281764212ed56
kangxh <kangxh@lordstar.net>
2015-08-19 15:39:48 +0800

    install

--- install.bat
+++ install.bat
@@ -3,3 +3,7 @@ mklink /H "%USERPROFILE%\.vimrc" ".vimrc"
 
 del "%USERPROFILE%\.ideavimrc"
 mklink /H "%USERPROFILE%\.ideavimrc" ".ideavimrc"
+
+git submodule init
+
+git submodule update
--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ rm -f ~/.vimrc
 ln -sf `pwd`/.vimrc ~/.vimrc
 rm -f ~/.ideavimrc
 ln -sf `pwd`/.ideavimrc ~/.ideavimrc
+
+git submodule init
+
+git submodule update
